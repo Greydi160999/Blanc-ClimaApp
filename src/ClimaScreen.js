7aeba14e65212fecdc4b03f5685dba291c5f85c4
chenaoh <moralesgreydi16@gmail.com>
2023-06-30 12:55:47 -0600

    Estilos de la App

--- src/ClimaScreen.js
+++ src/ClimaScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState} from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet} from 'react-native';
 import axios from 'axios';
 
 const countries = ['Belize', 'Honduras', 'Panama'];
@@ -12,6 +12,7 @@ useEffect(() => {
             const responsePromises = countries.map(country => 
                 axios.get(`https://api.weatherapi.com/v1/current.json?key=15a3cc60c2f64a52aa6170925233006&q=${country}&lang=es`)
 );
+
 const responses = await promise.all(responsePromises);
         const climaDataArray = responses.map(response => response.data);
         setClimaData(climaDataArray);
@@ -22,27 +23,50 @@ const responses = await promise.all(responsePromises);
       fetchClimaData();
 }, []);
 
-
 if (!climaData) {
     return (
-        <View>
-        <Text>Cargando ...  </Text>
+        <View style={styles.container}>
+        <Text style={styles.loadingText}> Cargando ...  </Text>
         </View>
     );
    }
 
  return (
-    <View>
+    <View style={styles.container}>
        {climaData.map((clima, index) => (
-        <View key={index}>
-           <Text>Ubicación: {clima.location.name},  {clima.location.region},  {clima.location.country}</Text>   
+        <View key={index} style={styles.climaContainer}>
+           <Text Text style={styles.text}>Ubicación: {clima.location.name},  {clima.location.region},  {clima.location.country}</Text>   
            <Text>Temperatura: {clima.current.temp_c}ºC</Text>
            <Text>Condición: {clima.current.condition.text}</Text>
+           <Text style={styles.text}>Viento: {clima.current.wind_kph} kph</Text>
+          <Text style={styles.text}>Humedad: {clima.current.humidity}%</Text>
            <Text>---------------------------------</Text>
             </View>
        ))}
     </View>
  );
 };
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 10,
+      backgroundColor: '#977ADB',
+    },
+    loadingText: {
+      fontSize: 18,
+      textAlign: 'center',
+    },
+    climaContainer: {
+      marginBottom: 20,
+    },
+    text: {
+      fontSize: 16,
+    },
+    separator: {
+      marginTop: 10,
+      textAlign: 'center',
+    },
+  });
 
 export default ClimaScreen;
